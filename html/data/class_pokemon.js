80b2a8b20a6d9149031fc82af2f8f61eb4a9b021
kararmel04 <armel.rouques056@gmail.com>
2026-05-11 14:37:01 +0200

    Ajout base classe pokemon

---
+++ html/data/class_pokemon.js
@@ -0,0 +1,14 @@
+class Pokemon {
+
+    constructor(id, name, stamina, attack, defense){
+        this.id = id;
+        this.name = name;
+        this.sta = stamina;
+        this.atk = attack;
+        this.def = defense;
+    }
+
+    toString(){
+        return `${name} :  #${id}, ${types}, ${sta}, ${atk}, ${def}, ${fast}, ${charged}`;
+    }
+}
