e881f337643d5b50a0a35ab8c6f8ed576335f17a
kararmel04 <armel.rouques056@gmail.com>
2026-05-11 15:12:24 +0200

    Ajout squelette test.js

---
+++ html/test/test.js
@@ -0,0 +1,27 @@
+console.log(`Liste des NN pokémons : `);
+console.log(` - <toString() du Nème pokémon>`);
+
+
+function getPokemonsByType(typeName){
+
+}
+
+function getPokemonsByAttack(attackName){
+
+}
+
+function getAttacksByType(typeName){
+
+}
+
+function sortPokemonsByTypeThenName(){
+    
+}
+
+function getWeakestEnemies(attackName){
+
+}
+
+function getBestFastAttacksForEnemy(print, pokemonName){
+    
+}
